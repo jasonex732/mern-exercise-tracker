ac3e39812775a3ece9527b22133f649a4dd19be2
Jason Eccles <jasonxeccles@gmail.com>
2020-06-10 08:56:36 -0400

    Updates

---
+++ precache-manifest.da099782dde0bd72139678a88e455ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363bbaeca1e2fbb507fc6cd24f6998a9",
+    "url": "/mern-exercise-tracker/index.html"
+  },
+  {
+    "revision": "8f8a1c95bf205234faf6",
+    "url": "/mern-exercise-tracker/static/css/2.bf5b17f7.chunk.css"
+  },
+  {
+    "revision": "8f8a1c95bf205234faf6",
+    "url": "/mern-exercise-tracker/static/js/2.e0a32118.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/mern-exercise-tracker/static/js/2.e0a32118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cc7dc580f90c9dcf71a",
+    "url": "/mern-exercise-tracker/static/js/main.65bab65b.chunk.js"
+  },
+  {
+    "revision": "eee6acd46b897e0a1930",
+    "url": "/mern-exercise-tracker/static/js/runtime-main.ed1d7ebb.js"
+  }
+]);
